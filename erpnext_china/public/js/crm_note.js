955b1039c6d10b8deabd58a7cdc0b4ae3cbabcb3
ilqc <iliuchao2326@163.com>
2025-02-19 16:09:11 +0800

    refactor: crm notes html

--- erpnext_china/public/js/crm_note.js
+++ erpnext_china/public/js/crm_note.js
@@ -9,10 +9,12 @@ erpnext.utils.CRMNotes = class CRMNotes {
       notes.sort(function(a, b) {
         return new Date(b.added_on) - new Date(a.added_on);
       });
+      
       let notes_html = frappe.render_template("crm_notes", {
         notes
       });
-      $(notes_html).appendTo(this.notes_wrapper);
+      const wrapper = expand_notes_html(this.frm, $(notes_html))
+      wrapper.appendTo(this.notes_wrapper);
       this.add_note();
       $(".notes-section").find(".edit-note-btn").on("click", function() {
         me.edit_note(this);
@@ -126,3 +128,94 @@ erpnext.utils.CRMNotes = class CRMNotes {
       });
     }
   };
+
+function expand_notes_html(frm, wrapper) {
+    let creationDatetime = moment(frm.doc.creation).format("YYYY-MM-DD HH:mm:ss");
+    wrapper.find(".revisit-create-date").first().text("创建时间：" + creationDatetime);
+
+    let revisitTimeBoxHtml = '';
+    for (let i = 1; i <= 5; i++) {
+        let times = '首次回访'
+        let lastElementClass = ''
+        let numBgClass = ''
+        let preDatetime = creationDatetime;
+        let expectedDatetime = creationDatetime;
+        switch (i) {
+            case 1:
+                preDatetime = creationDatetime;
+                expectedDatetime = addTohours(creationDatetime, 6)
+                break;
+            case 2:
+                preDatetime = addTohours(creationDatetime, 6);
+                expectedDatetime = addTohours(creationDatetime, 12);
+                times = "第二次回访"
+                break;
+            case 3:
+                preDatetime = addTohours(creationDatetime, 12);
+                expectedDatetime = addTohours(creationDatetime, 18);
+                times = "第三次回访"
+                break;
+            case 4:
+                preDatetime = addTohours(creationDatetime, 18);
+                expectedDatetime = addTohours(creationDatetime, 24);
+                times = "第四次回访"
+                break;
+            case 5:
+                preDatetime = addTohours(creationDatetime, 24);
+                expectedDatetime = addTohours(creationDatetime, 48);
+                times = "第五次回访"
+                lastElementClass = 'last'
+                break;
+        }
+        for(const note of frm.doc.notes) {
+            if('销售反馈' == note.custom_note_type) {
+                const code = getBgClass(note.added_on, preDatetime, expectedDatetime);
+                if(code == 1) {
+                    numBgClass = "success-bg-color";
+                    break
+                }
+                if(code == 0) {
+                    numBgClass = ''
+                    break
+                }
+                numBgClass = 'expired-bg-color'
+            }
+        }
+        
+        const timeBoxItem = `<div class="time-box-item">
+                <div class="time-title ${lastElementClass}">
+                    <div class="time-num ${numBgClass}">${i}</div>
+                </div>
+                <div class="time-body">
+                    <div class="times">${times}</div>
+                    <div class="time-description">请在${expectedDatetime}前完成</div>
+                </div>
+            </div>`
+        revisitTimeBoxHtml = revisitTimeBoxHtml + timeBoxItem
+    }
+    $(revisitTimeBoxHtml).appendTo(wrapper.find(".revisit-time-box").first())
+    return wrapper
+}
+
+function addTohours(d, h, f = 'YYYY-MM-DD HH:mm:ss') {
+    return moment(d).add(h, 'hours').format(f)
+}
+
+function getBgClass(targetDate, startDate, endDate) {
+    const target = new Date(targetDate);
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+
+    // 如果反馈时间落入区间内，则success
+    if (target >= start && target <= end) {
+        return 1
+    }
+
+    // 如果当前时间还没到此区间的结束时间
+    if (new Date() <= end) {
+        return 0
+    }
+    
+    // 其它情况都属于过期
+    return -1
+}
