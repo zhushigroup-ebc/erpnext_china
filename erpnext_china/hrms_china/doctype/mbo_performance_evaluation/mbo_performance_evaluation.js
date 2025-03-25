991a1fbafcbd3b094817fc1c596cad0480a83565
ilqc <iliuchao2326@163.com>
2025-03-25 17:18:16 +0800

    feat: update mbo

--- erpnext_china/hrms_china/doctype/mbo_performance_evaluation/mbo_performance_evaluation.js
+++ erpnext_china/hrms_china/doctype/mbo_performance_evaluation/mbo_performance_evaluation.js
@@ -2,24 +2,43 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("MBO Performance Evaluation", {
+
 	refresh(frm) {
+        if(["Pending", "月初目标确认-直属上级确认", "月初目标确认-被考核人确认"].includes(frm.doc.workflow_state)) {
+            frm.events.set_childfield_read_only(frm, ["expected_goal", "weighting", "evaluation_criteria"], 0);
+            frm.events.set_childfield_read_only(frm, ["actual_achievement", "score"], 1);
+        } else {
+            frm.events.set_childfield_read_only(frm, ["expected_goal", "weighting", "evaluation_criteria"], 1);
+            frm.events.set_childfield_read_only(frm, ["actual_achievement", "score"], 0);
+        }
+
+        if(["公司确认", "被考核人确认", "已确认"].includes(frm.doc.workflow_state)) {
+            frm.set_df_property("performance_evaluation_and_summary_form", "read_only", 1);
+            frm.set_df_property("section_break_hgzw", "hidden", 0);
+            frm.set_df_property("section_break_rsrt", "hidden", 0);
+        } else {
+            frm.set_df_property("performance_evaluation_and_summary_form", "read_only", 0);
+            frm.set_df_property("section_break_hgzw", "hidden", 1);
+            frm.set_df_property("section_break_rsrt", "hidden", 1);
+        }
+
         if (frm.doc.workflow_records.length > 0) {
             frm.events.make_workflow_html(frm)
         }
 	},
-
+    set_childfield_read_only(frm, table_fields, value, table_row_name = null ) {
+        table_fields.forEach(field=>{
+            frm.set_df_property("performance_evaluation_and_summary_form", "read_only", value, frm.doc.name, field);
+        })
+    },
     make_workflow_html(frm) {
         let workflow_record_html_wrapper = frm.fields_dict['workflow_record_html'].wrapper;
         $(workflow_record_html_wrapper).empty();
 
         const data = {}
-        frm.doc.workflow_records.sort((a, b) => {
-            const dateA = new Date(a.creation);
-            const dateB = new Date(b.creation);
-            return dateA - dateB; // 升序排序
-          });
-        frm.doc.workflow_records.reverse().forEach(item=>{
-            if (item.workflow_state != "Pending") {
+        frm.doc.workflow_records.reverse();
+        frm.doc.workflow_records.forEach(item=>{
+            if (item.workflow_state && item.workflow_state != "Pending") {
                 data[item.workflow_state] = item;
             }
         })
