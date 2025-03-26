da4ee7b0cf33ceb359acce87671384031b4257b6
digitwise <407469895@qq.com>
2025-03-26 19:11:46 +0800

    Update user.py

--- erpnext_china/erpnext_china/overrides/user/user.py
+++ erpnext_china/erpnext_china/overrides/user/user.py
@@ -2,5 +2,5 @@ import frappe
 
 @frappe.whitelist()
 def switch_theme(theme):
-	if theme in ["Dark", "Light", "Automatic", "Business"]:
+	if theme in ["Dark", "Light", "Automatic", "Ai Era"]:
 		frappe.db.set_value("User", frappe.session.user, "desk_theme", theme)
