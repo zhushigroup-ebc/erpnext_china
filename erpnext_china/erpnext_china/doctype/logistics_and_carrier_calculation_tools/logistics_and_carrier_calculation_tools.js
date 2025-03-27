6303ce91fe570972c680cf07fa03cc28a6e51e03
digitwise <407469895@qq.com>
2025-03-27 22:17:42 +0800

    增加计算运费工具doctype

---
+++ erpnext_china/erpnext_china/doctype/logistics_and_carrier_calculation_tools/logistics_and_carrier_calculation_tools.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Digitwise Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Logistics And Carrier Calculation Tools", {
+// 	refresh(frm) {
+
+// 	},
+// });
