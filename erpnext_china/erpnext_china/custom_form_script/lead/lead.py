e5fa3e366d2e63fb4a786867c0e45d0771342291
digitwise <407469895@qq.com>
2025-04-15 10:18:56 +0800

    线索有效判定

--- erpnext_china/erpnext_china/custom_form_script/lead/lead.py
+++ erpnext_china/erpnext_china/custom_form_script/lead/lead.py
@@ -6,6 +6,12 @@ from erpnext_china.utils.old_system_data import white_list, old_system_contacts
 from erpnext.crm.doctype.lead.lead import Lead
 import frappe.utils
 from erpnext_china.erpnext_china.custom_form_script.lead import auto_allocation
+import requests
+import json
+import os
+from tencentcloud.common import credential
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.cvm.v20170312 import cvm_client, models
 
 class CustomLead(Lead):
 	def create_contact(self):
@@ -198,6 +204,64 @@ class CustomLead(Lead):
 		
 		if self.is_new() and self.source in ["业务自录入","已合作客户补录"]:
 			self.status = "Lead"
+
+		def sse_client(content):
+			def get_token():
+				# 为了保护密钥安全，建议将密钥设置在环境变量中或者配置文件中，请参考本文凭证管理章节。
+				# 硬编码密钥到代码中有可能随代码泄露而暴露，有安全隐患，并不推荐。
+				# cred = credential.Credential("secretId", "secretKey")
+				secretKey = frappe.utils.password.get_decrypted_password('Tencent Settings',name='Tencent Settings',fieldname='api_key')
+				cred = credential.Credential(
+					"AKIDwuofP0PfhYDj3GH6rSxTsmZBc6OUSLBp",
+					secretKey)
+				client = cvm_client.CvmClient(cred, "ap-shanghai")
+			
+				req = models.DescribeInstancesRequest()
+				resp = client.DescribeInstances(req)
+			
+				return resp.to_json_string()
+
+			req_data = {
+				"content": f"""【{content}】0代表无效，1代表有效，仅输出0到1的数字""",
+				"bot_app_key": 'JsAUGOzE',
+				"visitor_biz_id": 'visitor_biz_id',
+				"session_id": json.loads(get_token())['RequestId'],
+			}
+
+			resp = requests.post("https://wss.lke.cloud.tencent.com/v1/qbot/chat/sse", data=json.dumps(req_data))
+			data = json.loads(resp.content.decode('utf-8').split("\n")[-3].replace('data:',''))['payload']['content']
+			return data
+
+		for note in self.notes:
+			if note.custom_note_type in ['销售反馈','客服反馈'] and  not note.custom_socre :
+				try:
+					# 从已有的评分中计算
+					socre = frappe.db.sql(f''' select sum(custom_socre)/count(custom_socre) as socre from `tabCRM Note` 
+							where custom_socre is not null
+							and note = '{note.note}' ''',as_dict=1)
+					socre = socre[0]['socre']
+					if socre == None:
+						pass
+					elif socre >=0.5:
+						socre = 1
+					elif 0 <= socre < 0.5:
+						socre = 0
+					else:
+						socre = None
+				except:
+					socre = None
+
+				if socre:
+					note.custom_socre = socre
+				else:
+					try:
+						socre = int(sse_client(note.note))
+					except:
+						pass
+				if socre == 1:
+					note.custom_useful = 1
+
+
 
 	def after_insert(self):
 		super().after_insert()
