e8212ef0e488363e7490e7c34cc5d85dd5862acc
digitwise <407469895@qq.com>
2026-03-19 19:22:53 +0800

    Revert "本地推"
    This reverts commit 8d2ed3a7aa41a9c1ed0faa9f78857d2cc9ea8901.

--- erpnext_china/erpnext_china/doctype/lead_domain_for_local/lead_domain_for_local.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Digitwise Ltd. and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Lead Domain for Local", {
-// 	refresh(frm) {
-
-// 	},
-// });
