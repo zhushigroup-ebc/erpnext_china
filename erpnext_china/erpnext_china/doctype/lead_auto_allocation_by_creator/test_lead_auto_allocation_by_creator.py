bb73c743ef4134dad1c63179d8862aba8049bc04
ilqc <iliuchao2326@163.com>
2026-04-13 20:48:23 +0800

    new

---
+++ erpnext_china/erpnext_china/doctype/lead_auto_allocation_by_creator/test_lead_auto_allocation_by_creator.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Digitwise Ltd. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeadAutoAllocationByCreator(FrappeTestCase):
+	pass
